4b6f8d18327de2532bfef48f36dd371d67682640
Caio Portella <caiohportella@gmail.com>
2024-07-30 16:05:11 -0300

    suspense error fix

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useTransition, useState } from "react";
+import React, { useRef, useTransition, useState, Suspense } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Loader2, Search } from "lucide-react";
@@ -21,38 +21,40 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="relative w-full h-14 flex flex-col bg-white">
-      <div className="relative h-14 z-10 rounded-md">
-        <Input
-          value={query}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              search();
-            }
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="relative w-full h-14 flex flex-col bg-white">
+        <div className="relative h-14 z-10 rounded-md">
+          <Input
+            value={query}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                search();
+              }
 
-            if (e.key === "Escape") {
-              inputRef?.current?.blur();
-            }
-          }}
-          ref={inputRef}
-          onChange={(e) => setQuery(e.target.value)}
-          disabled={isSearching}
-          className="absolute inset-0 h-full"
-        />
-        <Button
-          onClick={() => search()}
-          disabled={isSearching}
-          size="sm"
-          className="absolute right-0 inset-y-0 h-full rounded-l-none"
-        >
-          {isSearching ? (
-            <Loader2 className="h-6 w-6 animate-spin" />
-          ) : (
-            <Search />
-          )}
-        </Button>
+              if (e.key === "Escape") {
+                inputRef?.current?.blur();
+              }
+            }}
+            ref={inputRef}
+            onChange={(e) => setQuery(e.target.value)}
+            disabled={isSearching}
+            className="absolute inset-0 h-full"
+          />
+          <Button
+            onClick={() => search()}
+            disabled={isSearching}
+            size="sm"
+            className="absolute right-0 inset-y-0 h-full rounded-l-none"
+          >
+            {isSearching ? (
+              <Loader2 className="h-6 w-6 animate-spin" />
+            ) : (
+              <Search />
+            )}
+          </Button>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
